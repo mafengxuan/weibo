82088797d4bc97c3c427f31b7f5e552372c3ccc8
dingqianqian <2842961733@qq.com>
2017-07-05 20:20:06 +0800

    用户

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
 
 //后台登录页
 Route::get('/admin/login','Admin\LoginController@login');
+
 //处理登录
 Route::post('/admin/dologin','Admin\LoginController@dologin');
 
@@ -24,9 +25,14 @@ Route::post('/admin/dologin','Admin\LoginController@dologin');
 Route::get('/admin/code','Admin\LoginController@code');
 
 //后台模块
-Route::group([], function(){
+//Route::group(['prefix'=>'admin','namespace'=>'Admin','middleware'=>'admin.login'], function(){
+Route::group(['prefix'=>'admin','namespace'=>'Admin'], function(){
     //后台主页面
-    Route::resource('/admin/index', 'Admin\IndexController@index');
+    Route::resource('index', 'IndexController@index');
+    //退出登录
+    Route::get('quit','IndexController@quit');
     //后台欢迎页
-    Route::get('/admin/welcome','Admin\IndexController@welcome');
+    Route::get('welcome','IndexController@welcome');
+    //后台用户页面
+    Route::resource('user','UserController');
 });
